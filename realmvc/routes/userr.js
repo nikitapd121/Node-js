c85e3444fc708c53de9f3f89197491b6a1a94cdd
Nikita Prasad <nikitaprasad88@gmail.com>
2025-07-24 22:26:36 +0530

    Delete realmvc directory

--- realmvc/routes/userr.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controllers/store');
-
-router.get('/', homeController.getHomes);
-router.get('/booking',homeController.getBookings);
-router.get('/fav',homeController.favlist)
-router.get('/index',homeController.index)
-module.exports = router;
